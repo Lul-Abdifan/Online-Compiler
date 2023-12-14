ecb83de5fb56b85e315a1106de685b5b9bc9d70f
Lul-Abdifan <tasanbannu@gmail.com>
2023-12-14 22:49:07 +0300

    Update Editor style

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+// import ReactDOM from 'react-dom';
+
+import Editor from '@monaco-editor/react';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Editor</h1>
+      <Editor height="50vh" width="50%" defaultLanguage="javascript" defaultValue="// some comment" />;
     </div>
   );
 }
 
 export default App;
+
+
