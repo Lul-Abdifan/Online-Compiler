a6c1760bbb25ed9d1aef701a387d743cf56f99e7
Lul-Abdifan <tasanbannu@gmail.com>
2023-12-19 22:16:51 +0300

    Update language default values

--- src/language.js
+++ src/language.js
@@ -1,52 +1,52 @@
 export const files = {
-    'javascript': {
-      name: 'main.js',
-      language: 'javascript',
-      value: "someJSCodeExample",
-    },
-    'python': {
+  'javascript': {
+    name: 'main.js',
+    language: 'javascript',
+    value: "console.log('Hello, World!');",
+  },
+  'python': {
     name: 'main.py',
-      language: 'python',
-      value: "somePythonCodeExample",
-    },
-    'c++': {
-      name: 'main.cpp',
-      language: 'cpp',
-      value: "someCppCodeExample",
-    },
-    'java': {
-        name: 'main.java',
-        language: 'java',
-        value: "someJavaCodeExample",
-      },
-      'go': {
-        name: 'main.go',
-        language: 'go',
-        value: "someGoCodeExample",
-      },
-      'php': {
-        name: 'main.php',
-        language: 'php',
-        value: "somePhpCodeExample",
-      },
-      'ruby': {
-        name: 'main.rb',
-        language: 'ruby',
-        value: "someRCodeExample",
-      },
-      'rust': {
-        name: 'main.rs',
-        language: 'rust',
-        value: "someRustCodeExample",
-      },
-      'r': {
-        name: 'main.r',
-        language: 'r',
-        value: "someRCodeExample",
-      },
-      'swift': {
-        name: 'main.swift',
-        language: 'swift',
-        value: "someSwiftCodeExample",
-      },
-  };
+    language: 'python',
+    value: "print('Hello, World!')",
+  },
+  'c++': {
+    name: 'main.cpp',
+    language: 'cpp',
+    value: "#include <iostream>\n\nint main() {\n    std::cout << 'Hello, World!' << std::endl;\n    return 0;\n}",
+  },
+  'java': {
+    name: 'Main.java',
+    language: 'java',
+    value: "public class Main {\n    public static void main(String[] args) {\n        System.out.println('Hello, World!');\n    }\n}",
+  },
+  'go': {
+    name: 'main.go',
+    language: 'go',
+    value: "package main\n\nimport \"fmt\"\n\nfunc main() {\n    fmt.Println('Hello, World!')\n}",
+  },
+  'php': {
+    name: 'main.php',
+    language: 'php',
+    value: "<?php\n\necho 'Hello, World!';\n?>",
+  },
+  'ruby': {
+    name: 'main.rb',
+    language: 'ruby',
+    value: "puts 'Hello, World!'",
+  },
+  'rust': {
+    name: 'main.rs',
+    language: 'rust',
+    value: "fn main() {\n    println!('Hello, World!');\n}",
+  },
+  'r': {
+    name: 'main.r',
+    language: 'r',
+    value: "cat('Hello, World!')",
+  },
+  'swift': {
+    name: 'main.swift',
+    language: 'swift',
+    value: "print('Hello, World!')",
+  },
+};
